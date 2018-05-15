f8a385bb4b94b7f07450b590d16fedaba4d4baa9
Oliver Caldwell <olliec87@gmail.com>
2018-05-15 13:38:53 +0100

    Got discord connecting over lunch O_O

--- src/snowball/discord.clj
+++ src/snowball/discord.clj
@@ -1,5 +1,12 @@
 (ns snowball.discord
-  (:require [taoensso.timbre :as log]))
+  (:require [taoensso.timbre :as log])
+  (:import sx.blah.discord.api.ClientBuilder))
 
-(defn connect! [{:keys []}]
-  (log/info "Connecting to Discord"))
+;; https://github.com/Discord4J/Discord4J
+;; https://jitpack.io/com/github/Discord4J/Discord4J/2.10.1/javadoc/
+
+(defn connect! [{:keys [token]}]
+  (log/info "Connecting to Discord")
+  (let [client (ClientBuilder.)]
+    (.withToken client token)
+    (.login client)))
