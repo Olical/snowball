6a904c8748557406537048cd2608ac8756ddbeea
Oliver Caldwell <olliec87@gmail.com>
2018-05-18 15:13:54 +0100

    note down how I think the listening will work

--- src/snowball/discord.clj
+++ src/snowball/discord.clj
@@ -120,6 +120,14 @@
     {:receiver receiver
      :stream stream}))
 
+;; goal:
+;; store n bytes from every user in the channel right now
+;; it's a moving buffer so I drop bytes as more are pushed on
+;; every n ms (maybe 1s) that data is run through sphinx
+;; if the keyword is spotted that user gets focus and can speak into the google API or whatever
+;; when a keyword is seen we clear all buffers
+;; should probably use tritonus for audio stuff?
+
 (defn destroy-user-stream [{:keys [stream receiver]}]
   (let [am (audio-manager)]
     (.unsubscribeReceiver am receiver)
