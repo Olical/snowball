da748212fdff0160cfc29aedbe108785b727aa7b
Oliver Caldwell <olliec87@gmail.com>
2018-05-20 17:30:05 +0100

    First attempts at streaming data through sphinx

--- src/snowball/comprehension.clj
+++ src/snowball/comprehension.clj
@@ -1,20 +1,16 @@
 (ns snowball.comprehension
   (:require [taoensso.timbre :as log]
             [snowball.discord :as discord])
-  (:import [java.io File FileInputStream InputStream]
-           [edu.cmu.sphinx.api Configuration SpeechResult StreamSpeechRecognizer]))
+  (:import [java.io PipedInputStream PipedOutputStream]
+           [edu.cmu.sphinx.api Configuration StreamSpeechRecognizer]))
 
-;; goal:
-;; store n bytes from every user in the channel right now
-;; it's a moving buffer so I drop bytes as more are pushed on
-;; every n ms (maybe 1s) that data is run through sphinx
-;; if the keyword is spotted that user gets focus and can speak into the google API or whatever
-;; when a keyword is seen we clear all buffers
-;; should probably use tritonus for audio stuff?
+(defonce subscription! (atom nil)) 
 
-(defonce subscription! (atom nil))
+(defonce stream (PipedOutputStream.))
 
-(defn handle-audio! [{:keys [audio user]}])
+(defn handle-audio! [{:keys [user audio]}]
+  (when-not (discord/bot? user)
+    (.write stream audio 0 (count audio))))
 
 (defn init! []
   (when @subscription!
@@ -23,3 +19,18 @@
 
   (log/info "Subscribing to audio")
   (discord/subscribe-audio! #(handle-audio! %)))
+
+(comment
+  (def r (StreamSpeechRecognizer.
+           (doto (Configuration.)
+             (.setAcousticModelPath "resource:/edu/cmu/sphinx/models/en-us/en-us")
+             (.setDictionaryPath "resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict")
+             (.setLanguageModelPath "resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin"))))
+
+  (.startRecognition r (PipedInputStream. stream 1000))
+
+  (def res (.getResult r))
+
+  (.getHypothesis res)
+
+  (.stopRecognition r))
