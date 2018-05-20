dce938b0e32d39d2274b5c8d33917457a2579e69
Oliver Caldwell <olliec87@gmail.com>
2018-05-20 17:41:26 +0100

    Started booting sphinx, doesn't work though...

--- src/snowball/comprehension.clj
+++ src/snowball/comprehension.clj
@@ -4,33 +4,43 @@
   (:import [java.io PipedInputStream PipedOutputStream]
            [edu.cmu.sphinx.api Configuration StreamSpeechRecognizer]))
 
-(defonce subscription! (atom nil)) 
+(def stream-size 1000)
 
-(defonce stream (PipedOutputStream.))
+(defonce subscription! (atom nil)) 
+(defonce sphinx! (atom nil))
+(defonce stream! (atom nil))
 
 (defn handle-audio! [{:keys [user audio]}]
   (when-not (discord/bot? user)
-    (.write stream audio 0 (count audio))))
+    (.write @stream! audio 0 (count audio))))
 
 (defn init! []
   (when @subscription!
     (log/info "Unsubscribing from existing audio")
     (discord/unsubscribe-audio! @subscription!))
 
+  (when @sphinx!
+    (log/info "Stopping existing sphinx")
+    (.stopRecognition @sphinx!))
+
+  (when @stream!
+    (log/info "Closing existing audio stream")
+    (.close @stream!))
+
   (log/info "Subscribing to audio")
-  (discord/subscribe-audio! #(handle-audio! %)))
+  (reset! subscription! (discord/subscribe-audio! #(handle-audio! %)))
+
+  (log/info "Booting CMUSphinx")
+  (let [stream (PipedOutputStream.)
+        sphinx (StreamSpeechRecognizer.
+                 (doto (Configuration.)
+                   (.setAcousticModelPath "resource:/edu/cmu/sphinx/models/en-us/en-us")
+                   (.setDictionaryPath "resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict")
+                   (.setLanguageModelPath "resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin")))]
+    (reset! sphinx! sphinx)
+    (reset! stream! stream)
+    (.startRecognition sphinx (PipedInputStream. stream stream-size))))
 
 (comment
-  (def r (StreamSpeechRecognizer.
-           (doto (Configuration.)
-             (.setAcousticModelPath "resource:/edu/cmu/sphinx/models/en-us/en-us")
-             (.setDictionaryPath "resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict")
-             (.setLanguageModelPath "resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin"))))
-
-  (.startRecognition r (PipedInputStream. stream 1000))
-
-  (def res (.getResult r))
-
-  (.getHypothesis res)
-
-  (.stopRecognition r))
+  (def result (.getResult @sphinx!))
+  (.getHypothesis result))
