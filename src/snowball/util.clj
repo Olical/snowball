8597657e1168fa7b92847d3a15e96dce4918f0b5
Oliver Caldwell <olliec87@gmail.com>
2018-06-06 21:53:45 +0100

    Log out stack traces from poll errors

--- src/snowball/util.clj
+++ src/snowball/util.clj
@@ -7,10 +7,12 @@
             (pred-fn)
             (catch Exception e
               (log/error "Caught an error in poll-while pred" e)
+              (.printStackTrace e)
               true))
       (try
         (body-fn)
         (catch Exception e
-          (log/error "Caught an error in poll-while body" e)))
+          (log/error "Caught an error in poll-while body" e)
+          (.printStackTrace e)))
       (Thread/sleep poll-ms)
       (recur))))
