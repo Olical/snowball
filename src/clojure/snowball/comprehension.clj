04fe366479f045e2ae701d5d4a046f429be8a7f2
Oliver Caldwell <olliec87@gmail.com>
2018-09-17 22:47:05 +0100

    Transcription is working!

--- src/clojure/snowball/comprehension.clj
+++ src/clojure/snowball/comprehension.clj
@@ -7,7 +7,12 @@
             [snowball.stream :as stream]
             [snowball.util :as util]
             [snowball.speech :as speech])
-  (:import [snowball.porcupine Porcupine]))
+  (:import [snowball.porcupine Porcupine]
+           [com.google.protobuf ByteString]
+           [com.google.cloud.speech.v1p1beta1
+            SpeechClient
+            RecognitionConfig
+            RecognitionAudio]))
 
 (b/defcomponent phrase-audio-chan {:bounce/deps #{discord/audio-chan config/value}}
   (log/info "Starting phrase channel")
@@ -18,7 +23,7 @@
       ;; Pull a single audio packet out of Discord.
       ;; We receive one every 20ms and know which user it came from.
       (when-let [{:keys [user audio]} (a/<! discord/audio-chan)]
-        (try 
+        (try
           ;; Insert some blank state for the user if there isn't any.
           ;; This means it's a new phrase.
           (when-not (@state! user)
@@ -54,7 +59,7 @@
 (defn byte-pair->short [[a b]]
   (bit-or (bit-shift-left a 8) (bit-and b 0xFF)))
 
-(defn resampled-frames [byte-stream]
+(defn resample-for-porcupine [byte-stream]
   (->> byte-stream
        (stream/->bytes)
        (partition 2)
@@ -62,9 +67,24 @@
        (partition 512 512 (repeat 0))
        (map short-array)))
 
+(defn resample-for-google [byte-stream]
+  (->> byte-stream
+       (stream/->bytes)
+       (partition 2)
+       (sequence (comp (take-nth 6) (map reverse)))
+       (flatten)
+       (byte-array)))
+
 (b/defcomponent phrase-text-chan {:bounce/deps #{phrase-audio-chan speech/synthesiser}}
-  (log/info "Starting Porcupine")
-  (let [phrase-text-chan (a/chan (a/sliding-buffer 100))
+  (log/info "Starting speech to text systems")
+  (let [speech-client (.. SpeechClient (create))
+        recognition-config (.. RecognitionConfig
+                               (newBuilder)
+                               (setEncoding (.. com.google.cloud.speech.v1p1beta1.RecognitionConfig$AudioEncoding LINEAR16))
+                               (setSampleRateHertz 16000)
+                               (setLanguageCode "en-GB")
+                               (build))
+        phrase-text-chan (a/chan (a/sliding-buffer 100))
         porcupine (Porcupine. "wake-word-engine/Porcupine/lib/common/porcupine_params.pv"
                               "wake-word-engine/hey_snowball_linux.ppn"
                               0.5)
@@ -74,7 +94,7 @@
     (a/go-loop []
       (when-let [{:keys [user byte-stream]} (a/<! phrase-audio-chan)]
         (try
-          (let [frames (resampled-frames byte-stream)]
+          (let [frames (resample-for-porcupine byte-stream)]
             (when (some #(.processFrame porcupine %) frames)
               (let [user-name (discord/user->name user)
                     timeout-chan (a/timeout (get-in config/value [:comprehension :post-wake-timeout-ms]))]
@@ -86,7 +106,30 @@
                                     timeout-chan nil
                                     phrase-audio-chan ([phrase] phrase))]
                     (if (= user (:user phrase))
-                      (log/info "TODO Send audio off to Google speech to text.")
+                      (do
+                        ;; TODO Add context to speech to text so it can understand user and channel names.
+
+                        (log/info "Audio from" user-name "- sending to Google for speech recognition.")
+                        (let [proto-bytes (.. ByteString (copyFrom (resample-for-google (:byte-stream phrase))))
+                              recognition-audio (.. RecognitionAudio
+                                                    (newBuilder)
+                                                    (setContent proto-bytes)
+                                                    (build))
+                              results (-> (.. speech-client
+                                          (recognize recognition-config recognition-audio)
+                                          (getResultsList))
+                                          (.iterator)
+                                          (iterator-seq))]
+                          (if (seq results)
+                            (doseq [result results]
+                              (let [transcript (.. result (getAlternativesList) (get 0) (getTranscript))]
+                                (log/info  "Speech recognition transcript result:" transcript)
+
+                                ;; TODO Remove this, it's a temporary parrot thing.
+                                (speech/say! (str "I heard: " transcript))
+
+                                (a/>! phrase-text-chan {:user user, :phrase transcript})))
+                            (log/info "No results from Google speech recognition."))))
                       (recur))
                     (log/info user-name "didn't say anything after the wake word."))))))
           (catch Exception e
@@ -98,5 +141,5 @@
       (throw (Error. (str "Porcupine frame length and sample rate should be 512 / 16000, got " frame-length " / " sample-rate " instead!"))))
 
     (b/with-stop phrase-text-chan
-      (log/info "Shutting down Porcupine")
+      (log/info "Shutting down speech to text systems")
       (.delete porcupine))))
